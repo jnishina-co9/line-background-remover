6ad190576b3a3c4537d3c0b14d819e1422038209
Junko <jnishina.co9@gmail.com>
2026-05-30 20:44:24 +0900

    vite.config.jsの設定を更新し、spec.htmlとdemo.mp4を出力対象に追加

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/line-background-remover/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        spec: './spec.html'
+      }
+    }
+  }
 })
